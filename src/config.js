54d25305411c22555f639a3a44b8c460901dd9a4
yuvalotem <yuvalotem@gmail.com>
2020-10-19 10:29:01 +0300

    set up

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-upload-intro"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://i9i0mztuel.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_nCitTCgOF",
+      APP_CLIENT_ID: "5pd251js3b4jtj5qvqa9njbr88",
+      IDENTITY_POOL_ID: "us-east-2:fdb478dd-3b02-4166-8cb4-8d8bb25be8ce"
+    }
+  };
